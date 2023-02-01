a2e71a85e498e4912f36dbf0c889135b0e9adc60
xuxinyi <3314361808@qq.com>
2023-02-01 14:41:35 +0800

    集成了屏幕适配、二维码扫描、汉字转拼音工具、图片选择、文件选择和应用更新框架。

--- app/src/main/java/com/example/mvvmdemojava/ui/main/MainViewModel.java
+++ app/src/main/java/com/example/mvvmdemojava/ui/main/MainViewModel.java
@@ -3,10 +3,15 @@ package com.example.mvvmdemojava.ui.main;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Application;
+import android.content.Intent;
 import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.xiaoguang.widget.mlkitscanner.ScanManager;
+import com.xiaoguang.widget.mlkitscanner.callback.act.ScanCallback;
+import com.xuexiang.xupdate.XUpdate;
+
 import me.goldze.mvvmhabit.base.BaseViewModel;
 import me.goldze.mvvmhabit.binding.command.BindingAction;
 import me.goldze.mvvmhabit.binding.command.BindingCommand;
@@ -36,14 +41,31 @@ public class MainViewModel extends BaseViewModel {
     public BindingCommand testClickCommand1 = new BindingCommand(new BindingAction() {
         @Override
         public void call() {
-            Log.d("TagTest", "标题");
+            XUpdate.newBuild(getApplication())
+                    .updateUrl("https://gitee.com/xuexiangjys/XUpdate/raw/master/jsonapi/update_test.json")
+                    .update();
         }
     });
 
     public BindingCommand testClickCommand2 = new BindingCommand(new BindingAction() {
         @Override
         public void call() {
-            Log.d("TagTest", "左边");
+            ScanManager.startScan(AppManager.getAppManager().currentActivity(), new ScanCallback() {
+                @Override
+                public void onActivityResult(int resultCode, Intent data) {
+                    switch (resultCode) {
+                        case ScanManager.RESULT_SUCCESS:
+                            String resultSuccess = data.getStringExtra(ScanManager.INTENT_KEY_RESULT_SUCCESS);
+                            break;
+                        case ScanManager.RESULT_FAIL:
+                            String resultError = data.getStringExtra(ScanManager.INTENT_KEY_RESULT_ERROR);
+                            break;
+                        case ScanManager.RESULT_CANCLE:
+                            ToastUtils.showShort("取消扫码");
+                            break;
+                    }
+                }
+            });
         }
     });
 }
